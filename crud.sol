d211f8b61cb11737804b514a82f500d0dd0910a4
Mr-Dutchman <meelisfidelis@gmail.com>
2022-03-10 12:04:59 +0100

    crud operation

---
+++ crud.sol
@@ -0,0 +1,41 @@
+//SPDX License-Identifier :MIT
+
+pragma solidity ^0.8.0;
+
+contract Storage {
+	uint favoriteNumber;
+	bool favoriteBool;
+	string name;
+
+	struct people {
+		uint256 public favoriteNumber;
+		string name;
+	}
+	
+
+	People [] public people;
+	
+	//searching 
+	mapping( string => uint) public nameToFavoriteNumber;
+
+	//store
+	function store(uint _favoriteNumber) public view{
+
+		favoriteNumber = _favoriteNumber;
+	}
+
+	//get 
+	function retrive() public {
+		return favoriteName;
+
+	}
+
+	//add to array
+	function addPeople (uint256 favoriteNumber, string memory _name) public {
+		people.push(People(_favoriteNumber, _name));
+ 		nameToFavoriteNumber[_name] =_favoriteNumber
+	}
+}
+
+
+
