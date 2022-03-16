14f5e45755835e32686382b5a2772b25ff1cc3fb
Mr-Dutchman <meelisfidelis@gmail.com>
2022-03-16 13:00:22 +0100

    funding

--- fund.sol
+++ fund.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier : MIT
-pragma Solidity >=0.6.0 < 0.9.0;
-import '@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol'
+pragma solidity ^0.8.0;
+import '@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol';
+
 contract FundMe {
 
 	mapping(address => uint256) public addressToAmountFunded;
@@ -9,5 +10,25 @@ contract FundMe {
 		addressToAmountFunded[msg.sender] += msg.value;
 		
 	}
+
+	function getVersion()public view returns(uint256) {
+		AggregatorV3Interface priceFeed = AggregatorV3Interface();
+		return priceFeed.version();	
+	}
+
+	function getPrice() public view returns(uint256) {
+		AggregatorV3Interface priceFeed = AggregatorV3Interface();
+		(,int256 answer,,,) = priceFeed.latestRoundData();
+		return uint256(answer * 100000000);
+
+	}
+	
+	function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+		uint256 getPrice = getPrice();
+		uint256 amountInUsd = (getPrice * ethAmount)/ 1000000000000000000;
+		
+
+		return amountInUsd;
+	} 
 }
 
